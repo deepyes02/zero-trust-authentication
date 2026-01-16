034dae220ba9a7fce56ddadebe0f8bde763908df
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2026-01-16 13:48:33 +0900

    refactor: move root page and layout into `(pages)` route group for better organization.

--- frontend/src/app/(pages)/layout.tsx
+++ frontend/src/app/(pages)/layout.tsx
@@ -1,20 +1,18 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
-import "./globals.scss";
+import "../globals.scss"
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
   display: "swap",
 });
 
-import { AuthProvider } from "./_components/AuthProvider";
+import { AuthProvider } from "../_components/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Google Authentication",
   description: "Google Authentication",
 };
-
-
 
 export default function RootLayout({
   children,
